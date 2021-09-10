9bd756c693a642015f79872f62e0f54fe780fe33
gstawde <gargi.tawde01@gmail.com>
2021-09-10 14:55:23 -0700

    THIRD project related commit: 'Created javadoc comments for both classes, their constructors, and methods.'

--- src/edu/sjsu/tawde/Coffee.java
+++ src/edu/sjsu/tawde/Coffee.java
@@ -1,23 +1,39 @@
 package edu.sjsu.tawde;
 
+/**
+ * Coffee class which contains the minimum one attribute and main() method.
+ */
 public class Coffee {
 
     private String drinkName;
 
-    // CONSTRUCTOR(S)
+    /**
+     * Creates a Coffee object where default value for the class attribute is assigned ("Black Coffee")
+     */
     public Coffee() {
         this.drinkName = "Black Coffee";
     }
 
-    // METHOD(S)
-    public String toString() {
-        return this.drinkName;
-    }
-
+    /**
+     * Creates a Coffee object where attribute value is set to the value of the parameter drinkName that was passed in.
+     * @param drinkName A String that is passed in at the creation of a Coffee object. It represents the name of a drink of the user's choosing.
+     */
     public Coffee(String drinkName) {
         this.drinkName = drinkName;
     }
 
+    /**
+     * Takes value of attribute and returns (since it is already a String).
+     * @return Value of attribute drinkName in this object.
+     */
+    public String toString() {
+        return this.drinkName;
+    }
+
+    /**
+     * Method that tests the Coffee class through the creation of objects and use of methods the class gives access to.
+     * @param args A String array that you can pass into main(); e.g. if you use it on the command line.
+     */
     public static void main(String[] args) {
         Coffee inputTest = new Coffee("Macchiato");
         System.out.println("My favorite drink is a " + inputTest.toString());
--- src/edu/sjsu/tawde/Latte.java
+++ src/edu/sjsu/tawde/Latte.java
@@ -1,7 +1,14 @@
 package edu.sjsu.tawde;
 
+/**
+ * A class called Latte that has no attributes or methods of its own, aside from the main() method.
+ */
 public class Latte {
 
+    /**
+     * Method that tests the Coffee class through the creation of an object and use of methods the class gives access to.
+     * @param args
+     */
     public static void main(String[] args) {
         Coffee inLatteClass = new Coffee("Iced Caramel Latte");
         System.out.println(inLatteClass.toString());
