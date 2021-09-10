902795630374c8fc339af61cab54d49627d89230
gstawde <gargi.tawde01@gmail.com>
2021-09-09 18:12:54 -0700

    SECOND project related commit: 'Create another class and with a public static main method and create an object of the first class. Then print the value of the attribute.'

---
+++ src/edu/sjsu/tawde/Latte.java
@@ -0,0 +1,9 @@
+package edu.sjsu.tawde;
+
+public class Latte {
+
+    public static void main(String[] args) {
+        Coffee inLatteClass = new Coffee("Iced Caramel Latte");
+        System.out.println(inLatteClass.toString());
+    }
+}
