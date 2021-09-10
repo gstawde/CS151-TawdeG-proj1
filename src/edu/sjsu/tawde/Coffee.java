2d90f0fbf17982537f9b5b18856047974e3adda5
gstawde <gargi.tawde01@gmail.com>
2021-09-09 17:45:32 -0700

    FIRST project related commit: 'Create a relavent class attribute and constructor to initialize it.'

--- src/edu/sjsu/tawde/Coffee.java
+++ src/edu/sjsu/tawde/Coffee.java
@@ -2,7 +2,26 @@ package edu.sjsu.tawde;
 
 public class Coffee {
 
+    private String drinkName;
+
+    // CONSTRUCTOR(S)
+    public Coffee() {
+        this.drinkName = "Black Coffee";
+    }
+
+    // METHOD(S)
+    public String toString() {
+        return this.drinkName;
+    }
+
+    public Coffee(String drinkName) {
+        this.drinkName = drinkName;
+    }
+
     public static void main(String[] args) {
-        System.out.println("testing change");
+        Coffee inputTest = new Coffee("Macchiato");
+        System.out.println("My favorite drink is a " + inputTest.toString());
+        Coffee defaultTest = new Coffee();
+        System.out.println("The default drink is a " + defaultTest.toString());
     }
 }
